d2e25375931e47ae8dadb0c392c29f0e82d177e3
Rhys Collier <rhys.collier@hotmail.co.uk>
2019-05-23 11:39:58 +0100

    creates numbered list

--- modified_directory.rb
+++ modified_directory.rb
@@ -22,8 +22,8 @@ def print_header
   puts "-------------"
 end
 def print(students)
-  students.each do |student|
-    puts "#{student[:name]} (#{student[:cohort]} cohort)"
+  students.each.with_index do |student, idx|
+    puts "#{idx + 1}. #{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
 def print_footer(students)
