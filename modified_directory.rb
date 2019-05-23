870450b5fce0e1015d5e7f6b20f68fcac0de6167
Rhys Collier <rhys.collier@hotmail.co.uk>
2019-05-23 15:10:58 +0100

    checks against list of valid cohorts

--- modified_directory.rb
+++ modified_directory.rb
@@ -13,16 +13,34 @@ def input_students
     # ask for student's cohort
     student_num = students.count - 1
     puts "What is #{students[student_num][:name]}'s cohort?"
+    puts "Hit return if unknown"
     default_cohort = :unknown
     cohort = gets.chomp.downcase
     # assess if entered cohort is blank and if so assign it as the default
-    students[student_num][:cohort] = cohort.empty? ? default_cohort : cohort.to_sym
+    cohort = default_cohort if cohort.empty?
+    # keep asking for valid input if there's a typo
+    until valid_cohort(cohort.to_sym, default_cohort) do
+      puts "Please enter a valid cohort or hit return if unknown"
+      cohort = gets.chomp.downcase
+      cohort = default_cohort if cohort.empty?
+    end
+    students[student_num][:cohort] = cohort.to_sym
     puts "Now we have #{students.count} students"
     # get another name from the user
     name = gets.chomp
   end
   # return the array of students
   students
+end
+#checks for valid cohort input
+def valid_cohort(cohort, default)
+  cohorts = [
+    :january, :february, :march, :april,
+    :may, :june, :july, :august,
+    :september, :october, :november, :december,
+    default.to_sym
+  ]
+  cohorts.include?(cohort.to_sym)
 end
 def print_header(width)
   puts "The students of Villains Academy".center(width)
