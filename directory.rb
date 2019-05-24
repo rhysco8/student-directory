63d93b2992ae0fb685d8051f4dd5a39b041b4992
Rhys Collier <rhys.collier@hotmail.co.uk>
2019-05-24 13:20:48 +0100

    refactors adding student hash to list

--- directory.rb
+++ directory.rb
@@ -8,7 +8,7 @@ def input_students
   # while the name is not empty, repeat this code
   while !name.empty? do
     # add the student hash to the array
-    @students << {name: name, cohort: :november}
+    add_to_list(name, :november)
     puts "Now we have #{@students.count} students"
     # get another name from the user
     name = STDIN.gets.chomp
@@ -84,7 +84,7 @@ def load_students(filename = "students.csv")
   file = File.open(filename, "r")
   file.readlines.each do |line|
     name, cohort = line.chomp.split(',')
-    @students << {name: name, cohort: cohort.to_sym}
+    add_to_list(name, cohort)
   end
   file.close
 end
@@ -99,6 +99,10 @@ def try_load_students
     puts "Sorry, #{filename} doesn't exist."
     exit # quit the program
   end
+end
+
+def add_to_list(name, cohort)
+  @students << {name: name, cohort: cohort.to_sym}
 end
 
 try_load_students
